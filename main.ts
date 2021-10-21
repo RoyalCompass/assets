143d6a64055498084fa697940fdd179f055489cc
RoyalCompass <92768445+RoyalCompass@users.noreply.github.com>
2021-10-21 15:04:52 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . . . . c c . . . . . . . 
+    . . . . c c c 6 5 c 6 6 . . . . 
+    . . . . c 6 c 5 5 c 7 6 . . . . 
+    . . . 6 c c 7 5 5 7 c 6 6 . . . 
+    . . c c 7 7 7 7 7 5 7 7 c 6 . . 
+    . 6 6 6 c 6 7 7 7 7 6 c c 6 6 . 
+    c 7 7 7 6 c 7 c 6 7 6 7 7 7 7 6 
+    c c c 6 6 6 c 6 6 6 6 7 7 6 6 6 
+    . c c 7 6 6 6 6 6 7 7 7 7 c 6 . 
+    . c 7 7 6 6 7 6 6 7 7 6 7 7 c . 
+    . c c c c 7 7 6 f 7 7 c c c c . 
+    . . . . c 7 c f f c 7 c . . . . 
+    . . . . . 6 f e e e c . . . . . 
+    . . . . . e e e e e e . . . . . 
+    . . . . e e . e e . e e . . . . 
+    . . . . . . . e e . . . . . . . 
+    `, SpriteKind.Player)
