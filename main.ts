44b5c69db5c63332f8e6d8695d33670bf2bd2728
RoyalCompass <92768445+RoyalCompass@users.noreply.github.com>
2021-10-22 21:43:46 +1000

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,5 @@
-let mySprite = sprites.create(img`
-    f f f f f f f f f f f f f f f f 
-    b b b b b b b b b b b b b b b b 
-    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
-    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
-    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
-    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
-    a a a a a a a a a a a a a a a a 
-    d d d d d d d d d d d d d d d d 
-    c c c c c c c c c c c c c c c c 
-    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
-    5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 
-    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
-    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
-    e e e e e e e e e e e e e e e e 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
+let mySprite = sprites.create(assets.image`koala`, SpriteKind.Player)
+tiles.setTilemap(tilemap`mainTileMap`)
+controller.moveSprite(mySprite)
+scene.centerCameraAt(18, 110)
+scene.cameraFollowSprite(mySprite)
